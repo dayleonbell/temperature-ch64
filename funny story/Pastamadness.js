670071e879550950111f00b90394b8d94de4bfb5
dayleonbell <dontaedavis1@outlook.com>
2025-12-11 19:45:31 -0800

    functions

---
+++ funny story/Pastamadness.js
@@ -0,0 +1,29 @@
+let character1 = "Fandoogo the Racoon";
+let place1 = "dumpster at Little Cesars Pizza";
+let action1 = "looking for leftover pizza";
+let time1 = "midnight";
+
+const occaasion1 = "Garfield the Pastanator"
+
+console.log( "-- Pasta Madness --" );
+console.log( `One night, ${character1} was ${action1} in the ${place1} at ${time1}.` );
+console.log( `Suddenly, an alley cat appeared, attracted by the smell of freshly thrown away pasta.` );
+console.log('Fandoogo had to think on his feet to avoid a messy situation, seeing that there were only a couple bites of pasta left!');
+console.log(character1 + " tried to eat quickly when the cat yelled \"Hey! That's my dinner!\"");
+console.log(`Just then, ${occaasion1} appeared out of nowhere, wielding a giant fork and spoon!`);
+console.log('and said "Fear not, Fandoogo! "I shall show you how to consume this fine delicacy!"');
+console.log('And all the while ' + occaasion1 + ' spoke, ' + character1 + ' continued to scarf the pasta until it was gone.');
+
+//another version
+let character = "Golly the ghost";
+let place = "haunted burger joint";
+let action = "searching for the perfect burger";
+let time = "2 AM";
+
+const occaasion = "just cant find the right burger"
+
+console.log( "-- Burger Hunt --" );
+console.log( "One eerie night, " + character + " was " + action + " in the " + place + " at " + time + "." );
+console.log( "Suddenly, a shadowy figure appeared, drawn by the scent of sizzling patties." );
+console.log( character + " had to act fast to avoid a spooky situation, seeing that there was only one burger left!" );
+console.log( character + " took the biggest bite out of the burger when he realized that he" + occaasion);
